6b7ebed10864c2a5a5cadf05a088328b9ad22180
morganherlocker <morgan.herlocker@gmail.com>
2015-05-05 17:30:44 -0400

    first implementation

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-var 
-	fc = require('turf-featurecollection'),
-	pol = require('turf-polygon'),
-	earcut = require('earcut');
+var featurecollection = require('turf-featurecollection');
+var polygon = require('turf-polygon');
+var earcut = require('earcut');
 
 /**
-*	Function that process a {@link Polygon} and make a tin constrained to it using [earcut.js]{@link https://github.com/mapbox/earcut}
-* @requires	earcut.js
+* Function that process a {@link Polygon} and make a tin constrained to it using [earcut.js]{@link https://github.com/mapbox/earcut}
+* @requires earcut.js
 * @param {Feature<(Polygon)>} poly - single Polygon Feature
 * @return {FeatureCollection<(Polygon)>} 
-* @author	Abel Vázquez
-* @version 1.0.0
 */
 module.exports = function(poly){
+  if (!poly.geometry || poly.geometry.type !== 'Polygon' ) throw('input must be a polygon');
 
-	if (poly.geometry === void 0 || poly.geometry.type !== 'Polygon' ) throw('"turf-tesselate" only accepts polygon type input');
+  var data = flattenCoords(poly.geometry.coordinates);
+  var dim = 2;
+  var result = earcut(data.vertices, data.holes, dim);
 
-	var 
-		t = earcut(poly.geometry.coordinates),
-		p,
-		f=[];
+  var fc = featurecollection([]);
+  var vertices = [];
+  result.forEach(function(vert, i){
+    var index = result[i];
+    vertices.push([data.vertices[index * dim], data.vertices[index * dim + 1]]);
+  });
 
-		for (var i=0; i<t.length-2; i+=3){
-			p = [t[i], t[i+1],t[i+2], t[i]];
-			f.push(pol([p]));
-		}
-		
-	return fc(f);
-	
+  for (var i = 0; vertices && i < vertices.length; i += 3) {
+    var coords = vertices.slice(i, i + 3);
+    coords.push(vertices[i]);
+    fc.features.push(polygon([coords]));
+  }
+  
+  return fc;
+};
+
+function flattenCoords(data) {
+    var dim = data[0][0].length,
+        result = {vertices: [], holes: [], dimensions: dim},
+        holeIndex = 0;
+
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < data[i].length; j++) {
+            for (var d = 0; d < dim; d++) result.vertices.push(data[i][j][d]);
+        }
+        if (i > 0) {
+            holeIndex += data[i - 1].length;
+            result.holes.push(holeIndex);
+        }
+    }
+
+    return result;
 }
